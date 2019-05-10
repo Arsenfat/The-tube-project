17f97f86b73e97b8536ae8b6576949268278ec11
Guillaume Boutry <boutryguillaume1@gmail.com>
2019-05-10 09:56:55 +0100

    Updated Java version, Set file Encoding, Added dependency to JGRAPHT

--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -2,6 +2,7 @@ package com.tubeproject.core;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Salut John");
+        System.out.println("Salut Sophie");
     }
+
 }
