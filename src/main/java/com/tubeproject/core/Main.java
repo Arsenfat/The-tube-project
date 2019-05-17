e345c5285a3051014304e58e2ab0f4e6a657f2b7
Nightglowie <sophie.pages8@orange.fr>
2019-05-17 12:10:57 +0100

    Creation first windom with javaFX

--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -1,8 +1,11 @@
 package com.tubeproject.core;
 
+import com.tubeproject.view.ViewMainScreen;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Salut Sophie");
+        ViewMainScreen.startWindow();
     }
 
 }
