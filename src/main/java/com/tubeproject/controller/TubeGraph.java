532b1541be16c77e9925a7e257518e0b3797d7f7
arsenfat <rayonjonathan@yahoo.fr>
2019-05-18 00:12:38 +0100

    First graph creation and add of edge class

--- src/main/java/com/tubeproject/controller/TubeGraph.java
+++ src/main/java/com/tubeproject/controller/TubeGraph.java
@@ -1,24 +1,33 @@
 package com.tubeproject.controller;
 
-import org.jgrapht.graph.AsWeightedGraph;
+import java.util.LinkedList;
 
 public class TubeGraph {
 
-    private AsWeightedGraph graph;
+    private int vertices;
+    private LinkedList<Edge>[] adjacencylist;
 
-    public TubeGraph(AsWeightedGraph graph) {
-        this.graph = graph;
+    public TubeGraph(int vertices) {
+        this.vertices = vertices;
+        adjacencylist = new LinkedList[vertices];
+        //initialize adjacency lists for all the vertices
+        for (int i = 0; i <vertices ; i++) {
+            adjacencylist[i] = new LinkedList<>();
+        }
     }
 
-    public AsWeightedGraph makeGraph() {
-        return graph;
+    public void addEdge(int source, int destination, int weight) {
+        Edge edge = new Edge(source, destination, weight);
+        adjacencylist[source].addFirst(edge); //for directed graph
     }
 
-    public AsWeightedGraph getGraph() {
-        return graph;
-    }
-
-    public void setGraph(AsWeightedGraph graph) {
-        this.graph = graph;
+    public void printGraph(){
+        for (int i = 0; i <vertices ; i++) {
+            LinkedList<Edge> list = adjacencylist[i];
+            for (int j = 0; j <list.size() ; j++) {
+                System.out.println("vertex-" + i + " is connected to " +
+                        list.get(j).getDestination() + " with weight " +  list.get(j).getWeight());
+            }
+        }
     }
 }
--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -1,8 +1,24 @@
 package com.tubeproject.core;
 
+import com.tubeproject.controller.TubeGraph;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Salut Sophie");
+
+        int vertices = 6;
+        TubeGraph graph = new TubeGraph(vertices);
+        graph.addEdge(0, 1, 4);
+        graph.addEdge(0, 2, 3);
+        graph.addEdge(1, 3, 2);
+        graph.addEdge(1, 2, 5);
+        graph.addEdge(2, 3, 7);
+        graph.addEdge(3, 4, 2);
+        graph.addEdge(4, 0, 4);
+        graph.addEdge(4, 1, 4);
+        graph.addEdge(4, 5, 6);
+        graph.printGraph();
+
     }
 
 }
