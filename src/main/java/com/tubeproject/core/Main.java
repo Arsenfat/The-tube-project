fe7de1d7b8460529835be4f32b5fab9f2f28bcae
Nightglowie <sophie.pages8@orange.fr>
2019-05-17 12:10:57 +0100

    Creation first windom with javaFX

--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -1,33 +1,11 @@
 package com.tubeproject.core;
 
+import com.tubeproject.view.ViewMainScreen;
+
 public class Main {
-    public static void main(String[] args) {
-        /*DatabaseConnection.DatabaseOpen();
-        User user = new User("Jean",
-                "Kalash",
-                Date.valueOf(LocalDate.of(1992, Month.MAY, 31)),
-                "jean.kalash@gmail.com",
-                "patrick",
-                1);
-        user.crypt();
-        UserRequest uR = new UserRequest(user);
-        Insert insert = new Insert(uR);
-        insert.insert();
-
-
-        LoginRequest lR = new LoginRequest(user.getEmail());
-        Select select = new Select(lR);
-        User u = (User) (select.select().get());
-        boolean t = Login.checkPassword("patrick", u.getPassword(), u.getSalt());
-        if(t){
-            System.out.println("BIENVENUE !");
-        } else {
-            System.out.println("PARS SALE HACKEUR");
-        }
-
-
-        DatabaseConnection.DatabaseClose();
-*/
+    public static void main(String[] args){
+        System.out.println("Salut Sophie");
+        ViewMainScreen.startWindow();
     }
 
 }
