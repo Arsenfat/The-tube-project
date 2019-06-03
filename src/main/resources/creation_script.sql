9114f14ba65ba2bfe26b21b99cef0d2faaee104a
Guillaume Boutry <boutryguillaume1@gmail.com>
2019-06-03 18:21:07 +0100

    Changed DB Scheme, inserted station, lines, durations, zones

--- src/main/resources/creation_script.sql
+++ src/main/resources/creation_script.sql
@@ -1,53 +1,58 @@
 CREATE TABLE `lines`
 (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `stations`
 (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `naptan` varchar(12) PRIMARY KEY,
   `name` varchar(255) NOT NULL,
-  `wheelchair` boolean,
-  `zone` int
+  `wheelchair` boolean
 );
 
 CREATE TABLE `line_stations`
 (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `line` int,
-  `station` int
+  `station` varchar(12),
+  FOREIGN KEY (line) REFERENCES `lines`(id),
+  FOREIGN KEY (station) REFERENCES `stations`(naptan)
 );
 
 
 CREATE TABLE `zones`
 (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
+  `name` VARCHAR(255)
 );
 
 CREATE TABLE `zones_stations`
 (
     `zone` int,
-    `station` int,
+    `station` VARCHAR(12),
     CONSTRAINT PRIMARY KEY (zone, station),
-    CONSTRAINT FOREIGN KEY (zone) REFERENCES zones(id),
-    CONSTRAINT FOREIGN KEY (station) REFERENCES stations(id)
+    CONSTRAINT FOREIGN KEY (zone) REFERENCES `zones`(id),
+    CONSTRAINT FOREIGN KEY (station) REFERENCES `stations`(naptan)
 );
 
 CREATE TABLE `station_durations`
 (
-  `station_departing` int NOT NULL,
-  `station_arriving` int NOT NULL,
-  `duration` time NOT NULL,
-  CONSTRAINT PRIMARY KEY (station_departing, station_arriving)
+  `station_departing` VARCHAR(12) NOT NULL,
+  `station_arriving` VARCHAR(12) NOT NULL,
+  `duration` double NOT NULL,
+  CONSTRAINT PRIMARY KEY (station_departing, station_arriving),
+  CONSTRAINT FOREIGN KEY (station_departing) REFERENCES `stations`(naptan),
+  CONSTRAINT FOREIGN KEY (station_arriving) REFERENCES `stations`(naptan)
 );
 
 CREATE TABLE `connections`
 (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `station_one` int NOT NULL,
-  `station_two` int NOT NULL
+  `station_one` VARCHAR(12) NOT NULL,
+  `station_two` VARCHAR(12) NOT NULL,
+  CONSTRAINT FOREIGN KEY (station_one) REFERENCES `stations`(naptan),
+  CONSTRAINT FOREIGN KEY (station_two) REFERENCES `stations`(naptan)
 );
 
 CREATE TABLE `fares`
@@ -55,8 +60,29 @@ CREATE TABLE `fares`
   `departing_zone` int,
   `arriving_zone` int,
   `price` decimal,
-  CONSTRAINT PRIMARY KEY (departing_zone, arriving_zone)
+  CONSTRAINT PRIMARY KEY (departing_zone, arriving_zone),
+  CONSTRAINT FOREIGN KEY (departing_zone) REFERENCES `zones`(id),
+  CONSTRAINT FOREIGN KEY (arriving_zone) REFERENCES `zones`(id)
 );
+
+CREATE TABLE `user_addresses`
+(
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `street_number` varchar(255) NOT NULL,
+  `street_name` varchar(255) NOT NULL,
+  `postal_code` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `nearest_station` VARCHAR(12),
+  CONSTRAINT FOREIGN KEY (nearest_station) REFERENCES `stations`(naptan)
+);
+
+CREATE TABLE `roles`
+(
+  `id` int PRIMARY KEY,
+  `name` varchar(255) NOT NULL
+);
+
 
 CREATE TABLE `users`
 (
@@ -68,73 +94,29 @@ CREATE TABLE `users`
   `password` varchar(255) NOT NULL,
   `home` int,
   `work` int,
-  `role` int
+  `role` int,
+  CONSTRAINT FOREIGN KEY (home) REFERENCES `user_addresses`(id),
+  CONSTRAINT FOREIGN KEY (work) REFERENCES `user_addresses`(id),
+  CONSTRAINT FOREIGN KEY (role) REFERENCES `roles`(id)
 );
 
-CREATE TABLE `roles`
-(
-  `id` int PRIMARY KEY,
-  `name` varchar(255) NOT NULL
-);
-
-CREATE TABLE `user_addresses`
-(
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `street_number` varchar(255) NOT NULL,
-  `street_name` varchar(255) NOT NULL,
-  `postal_code` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `label` varchar(255) NOT NULL,
-  `nearest_station` int
-);
 
 CREATE TABLE `travels`
 (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `starting_station` int NOT NULL,
-  `arriving_station` int NOT NULL,
-  `user_id` int NOT NULL
+  `starting_station` VARCHAR(12) NOT NULL,
+  `arriving_station` VARCHAR(12) NOT NULL,
+  `user_id` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (starting_station) REFERENCES `stations`(naptan),
+  CONSTRAINT FOREIGN KEY (arriving_station) REFERENCES `stations`(naptan),
+  CONSTRAINT FOREIGN KEY (user_id) REFERENCES `users`(id)
 );
 
 CREATE TABLE `histories`
 (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `travel_id` int NOT NULL,
-  `station` int NOT NULL
+  `station` varchar(12) NOT NULL,
+  CONSTRAINT FOREIGN KEY (travel_id) REFERENCES `travels`(id),
+  CONSTRAINT FOREIGN KEY (station) REFERENCES `stations`(naptan)
 );
-
-ALTER TABLE `stations` ADD FOREIGN KEY (`zone`) REFERENCES `zones` (`id`);
-
-ALTER TABLE `line_stations` ADD FOREIGN KEY (`line`) REFERENCES `lines` (`id`);
-
-ALTER TABLE `line_stations` ADD FOREIGN KEY (`station`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `station_durations` ADD FOREIGN KEY (`station_departing`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `station_durations` ADD FOREIGN KEY (`station_arriving`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `connections` ADD FOREIGN KEY (`station_one`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `connections` ADD FOREIGN KEY (`station_two`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `fares` ADD FOREIGN KEY (`departing_zone`) REFERENCES `zones` (`id`);
-
-ALTER TABLE `fares` ADD FOREIGN KEY (`arriving_zone`) REFERENCES `zones` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`home`) REFERENCES `user_addresses` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`work`) REFERENCES `user_addresses` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`role`) REFERENCES `roles` (`id`);
-
-ALTER TABLE `user_addresses` ADD FOREIGN KEY (`nearest_station`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `travels` ADD FOREIGN KEY (`starting_station`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `travels` ADD FOREIGN KEY (`arriving_station`) REFERENCES `stations` (`id`);
-
-ALTER TABLE `travels` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `histories` ADD FOREIGN KEY (`travel_id`) REFERENCES `travels` (`id`);
-
-ALTER TABLE `histories` ADD FOREIGN KEY (`station`) REFERENCES `stations` (`id`);
