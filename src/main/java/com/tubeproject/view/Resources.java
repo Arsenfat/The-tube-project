4c098858e1b3b98cf7ddf42f0e94250833284b77
Nightglowie <sophie.pages8@orange.fr>
2019-06-10 08:42:20 +0100

    Creation first windom with javaFX

---
+++ src/main/java/com/tubeproject/view/Resources.java
@@ -0,0 +1,14 @@
+package com.tubeproject.view;
+
+public class Resources {
+    public static class ViewFiles {
+        public static final String LOGIN_SCREEN = "/view/LoginScreen.fxml";
+        public static final String MAIN_SCREEN = "/view/ViewMainScreen.fxml";
+    }
+
+    public static class Images {
+        public static final String BACKGROUND = "/img/london-underground-not-floue.jpg";
+        public static final String LOGO = "/img/LogoUK.png";
+        public static final String LOGO1 = "/img/logo1.png";
+    }
+}
