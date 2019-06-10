06b827056e46e22cb41da6ea495963a3db70e054
Nightglowie <sophie.pages8@orange.fr>
2019-06-10 08:42:20 +0100

    Creation first windom with javaFX

---
+++ src/main/java/com/tubeproject/view/LoginScreen.java
@@ -0,0 +1,53 @@
+package com.tubeproject.view;
+
+import com.tubeproject.utils.FXMLUtils;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreen extends Application implements Initializable {
+
+    @FXML
+    private ImageView imgView;
+
+
+    public static void startWindow() {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane anchorPane = FXMLUtils.loadFXML(Resources.ViewFiles.LOGIN_SCREEN);
+
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeImgView();
+    }
+
+    private void initializeImgView() {
+        InputStream stream = getClass().getResourceAsStream(Resources.Images.LOGO1);
+        Image img = new Image(stream);
+        imgView.setImage(img);
+    }
+}
