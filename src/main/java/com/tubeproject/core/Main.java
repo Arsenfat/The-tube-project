00fedca7a8e55068e67fb7d2e3d403832f6dc8be
Nightglowie <sophie.pages8@orange.fr>
2019-06-12 20:55:31 +0100

    Added Screens, Resources

--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -1,12 +1,12 @@
 package com.tubeproject.core;
 
 import com.tubeproject.view.LoginScreen;
+import com.tubeproject.view.SignUpScreen;
 import com.tubeproject.view.ViewMainScreen;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Salut Sophie");
-        LoginScreen.startWindow();
+        SignUpScreen.startWindow();
     }
 
 }
--- src/main/java/com/tubeproject/view/Resources.java
+++ src/main/java/com/tubeproject/view/Resources.java
@@ -4,11 +4,15 @@ public class Resources {
     public static class ViewFiles {
         public static final String LOGIN_SCREEN = "/view/LoginScreen.fxml";
         public static final String MAIN_SCREEN = "/view/ViewMainScreen.fxml";
+        public static final String SIGN_UP_SCREEN = "/view/SignUpScreen.fxml";
     }
 
     public static class Images {
         public static final String BACKGROUND = "/img/london-underground-not-floue.jpg";
         public static final String LOGO = "/img/LogoUK.png";
         public static final String LOGO1 = "/img/logo1.png";
+        public static final String PASSWORD = "/img/password.png";
+        public static final String USERNAME = "/img/username.png";
+        public static final String LONDON = "/img/london.png";
     }
 }
