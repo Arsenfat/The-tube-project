61151bd1db5d145c16fe820db2c026b0ca2f3a69
Nightglowie <sophie.pages8@orange.fr>
2019-06-13 13:58:21 +0100

    changing stages

--- src/main/java/com/tubeproject/view/LoginScreen.java
+++ src/main/java/com/tubeproject/view/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.tubeproject.view;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import com.tubeproject.utils.FXMLUtils;
 import javafx.application.Application;
@@ -7,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -15,7 +17,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.awt.event.MouseEvent;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -35,7 +39,50 @@ public class LoginScreen extends Application implements Initializable {
     private AnchorPane anchorPane;
 
     @FXML
-    private JFXTextField txtUsername;
+    private JFXButton facebookIcon;
+
+    @FXML
+    private JFXButton twitterIcon;
+
+    @FXML
+    private JFXButton instagramIcon;
+
+    @FXML
+    private JFXButton mailIcon;
+
+    @FXML
+    private void handleButtonActionHomePage() {
+        System.out.println("you've clicked");
+        AnchorPane homePage;
+        try {
+            homePage = FXMLLoader.load(getClass().getResource(Resources.ViewFiles.MAIN_SCREEN));
+
+        } catch (IOException e) {
+            System.out.println("Warning unandled exeption.");
+            return;
+        }
+        Scene homeScene = new Scene(homePage);
+        Stage homeStage = (Stage) anchorPane.getScene().getWindow();
+        homeStage.setScene(homeScene);
+        homeStage.show();
+    }
+
+
+    @FXML
+    private void handleButtonActionSignUp(ActionEvent event) {
+        AnchorPane signUpPage;
+        try {
+            signUpPage = FXMLLoader.load(getClass().getResource(Resources.ViewFiles.SIGN_UP_SCREEN));
+
+        } catch (IOException e) {
+            System.out.println("Warning unandle exeption.");
+            return;
+        }
+        Scene signUpScene = new Scene(signUpPage);
+        Stage signUpStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        signUpStage.setScene(signUpScene);
+        signUpStage.show();
+    }
 
     public static void startWindow() {
         launch();
@@ -55,6 +102,7 @@ public class LoginScreen extends Application implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initializeImgView();
         initializeBackground();
+        initializeIcons();
     }
 
     private void initializeImgView() {
@@ -75,5 +123,32 @@ public class LoginScreen extends Application implements Initializable {
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, false, false);
         BackgroundImage backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         anchorPane.setBackground(new Background(backgroundImage));
+    }
+
+    private void initializeIcons() {
+        InputStream stream = getClass().getResourceAsStream(Resources.Images.FACEBOOK);
+        Image img = new Image(stream);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        BackgroundImage backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        facebookIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.TWITTER);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        twitterIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.INSTAGRAM);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        instagramIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.MAIL);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        mailIcon.setBackground(new Background(backgroundImage));
+
     }
 }
--- src/main/java/com/tubeproject/view/SignUpScreen.java
+++ src/main/java/com/tubeproject/view/SignUpScreen.java
@@ -1,5 +1,6 @@
 package com.tubeproject.view;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import com.tubeproject.utils.FXMLUtils;
 import javafx.application.Application;
@@ -7,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -16,6 +18,7 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,6 +36,35 @@ public class SignUpScreen extends Application implements Initializable {
 
     @FXML
     private JFXTextField txtUsername;
+
+    @FXML
+    private JFXButton facebookIcon;
+
+    @FXML
+    private JFXButton twitterIcon;
+
+    @FXML
+    private JFXButton instagramIcon;
+
+    @FXML
+    private JFXButton mailIcon;
+
+    @FXML
+    private void handleButtonActionHomePage() {
+        System.out.println("you've clicked");
+        AnchorPane homePage;
+        try {
+            homePage = FXMLLoader.load(getClass().getResource(Resources.ViewFiles.MAIN_SCREEN));
+
+        } catch (IOException e) {
+            System.out.println("Warning unandled exeption.");
+            return;
+        }
+        Scene homeScene = new Scene(homePage);
+        Stage homeStage = (Stage) anchorPane.getScene().getWindow();
+        homeStage.setScene(homeScene);
+        homeStage.show();
+    }
 
     public static void startWindow() {
         launch();
@@ -52,6 +84,7 @@ public class SignUpScreen extends Application implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initializeImgView();
         initializeBackground();
+        initializeIcons();
     }
 
     private void initializeImgView() {
@@ -66,5 +99,32 @@ public class SignUpScreen extends Application implements Initializable {
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, false, false);
         BackgroundImage backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         anchorPane.setBackground(new Background(backgroundImage));
+    }
+
+    private void initializeIcons() {
+        InputStream stream = getClass().getResourceAsStream(Resources.Images.FACEBOOK);
+        Image img = new Image(stream);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        BackgroundImage backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        facebookIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.TWITTER);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        twitterIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.INSTAGRAM);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        instagramIcon.setBackground(new Background(backgroundImage));
+
+        stream = getClass().getResourceAsStream(Resources.Images.MAIL);
+        img = new Image(stream);
+        backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        backgroundImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        mailIcon.setBackground(new Background(backgroundImage));
+
     }
 }
