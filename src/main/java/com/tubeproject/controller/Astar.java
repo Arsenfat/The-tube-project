028ce0d11cf4966a7aaab4e31e79b83024854c83
arsenfat <rayonjonathan@yahoo.fr>
2019-07-09 14:22:37 +0100

    Algo integration

---
+++ src/main/java/com/tubeproject/controller/Astar.java
@@ -0,0 +1,107 @@
+package com.tubeproject.controller;
+import java.util.PriorityQueue;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Astar{
+
+    public static List<Node> printPath(Node target){
+        List<Node> path = new ArrayList<Node>();
+
+        for(Node node = target; node!=null; node = node.getParent()){
+            path.add(node);
+        }
+
+        Collections.reverse(path);
+
+        return path;
+    }
+
+    //search the shortest path by taking the departure node and the end node
+    public static void AstarSearch(Node source, Node goal){
+
+        Set<Node> explored = new HashSet<Node>();
+
+        PriorityQueue<Node> queue = new PriorityQueue<Node>(20,
+                new Comparator<Node>(){
+                    //override compare method
+                    public int compare(Node i, Node j){
+                        if(i.getF_scores() > j.getF_scores()){
+                            return 1;
+                        }
+
+                        else if (i.getF_scores() < j.getF_scores()){
+                            return -1;
+                        }
+
+                        else{
+                            return 0;
+                        }
+                    }
+
+                }
+        );
+
+        //cost from start is 0 since no path and predecessors yet
+        source.setG_scores(0);
+
+        queue.add(source);
+
+        boolean found = false;
+
+        //we continue while the open list is not empty and goal has not been found
+        while((!queue.isEmpty())&&(!found)){
+
+            //the node in having the lowest f_score value
+            Node current = queue.poll();
+
+            explored.add(current);
+
+            //if we found the goal we stop
+            if(current.getValue().equals(goal.getValue())){
+                found = true;
+            }
+
+            //check every neighbour of current node
+            for(Edge e : current.getAdjacencies()){
+                Node child = e.getTarget();
+                double cost = e.getCost();
+                double temp_g_scores = current.getG_scores() + cost;
+                double temp_f_scores = temp_g_scores + child.getH_scores(goal.getLatitude(), goal.getLongitude());
+                System.out.println(child.toString() + " wihth h value: " + child.getH_scores(goal.getLatitude(), goal.getLongitude()) + " and f value:" + temp_f_scores );
+
+                //if the new f cost is higher then we skip it
+
+                if((explored.contains(child)) &&
+                        (temp_f_scores >= child.getF_scores())){
+                    continue;
+                }
+
+                //if the child is not in open list or f score is lower
+
+                else if((!queue.contains(child)) ||
+                        (temp_f_scores < child.getF_scores())){
+
+                    child.setParent(current);
+                    child.setG_scores(temp_g_scores);
+                    child.setF_scores(temp_f_scores);
+
+                    if(queue.contains(child)){
+                        queue.remove(child);
+                    }
+
+                    queue.add(child);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
--- src/main/java/com/tubeproject/core/Main.java
+++ src/main/java/com/tubeproject/core/Main.java
@@ -1,8 +1,12 @@
 package com.tubeproject.core;
 
+import com.tubeproject.controller.Astar;
+import com.tubeproject.controller.Edge;
+import com.tubeproject.controller.Node;
 import com.tubeproject.model.ContextMap;
 import com.tubeproject.view.ViewMainScreen;
 
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -10,6 +14,71 @@ public class Main {
         //Toujours initialiser la context map
         Map<String, Object> ctxMap = ContextMap.getContextMap();
         ViewMainScreen.startWindow();
+
+        Astar aStar = new Astar();
+
+
+        //initialize the graph base on the example graph
+        Node n0 = new Node("Station 0", 0, 1);
+        Node n1 = new Node("Station 1", 1, 2);
+        Node n2 = new Node("Station 2", 1, 0);
+        Node n3 = new Node("Station 3", 2, 2);
+        Node n4 = new Node("Station 4", 2, 0);
+        Node n5 = new Node("Station 5", 3, 1);
+
+        //initialize the edges
+
+        //Station 0
+        n0.setAdjacencies(new Edge[]{
+                new Edge(n1,4),
+                new Edge(n2,3),
+                new Edge(n4,4)
+        });
+
+        //Station 1
+        n1.setAdjacencies(new Edge[]{
+                new Edge(n0,4),
+                new Edge(n2,5),
+                new Edge(n3,2),
+                new Edge(n4,4)
+        });
+
+
+        //Station 2
+        n2.setAdjacencies(new Edge[]{
+                new Edge(n0,3),
+                new Edge(n1,5),
+                new Edge(n3,7)
+        });
+
+        //Station 3
+        n3.setAdjacencies(new Edge[]{
+                new Edge(n1,2),
+                new Edge(n2,7),
+                new Edge(n4,2)
+        });
+
+
+        //Station 4
+        n4.setAdjacencies(new Edge[]{
+                new Edge(n0,4),
+                new Edge(n1,4),
+                new Edge(n3,2),
+                new Edge(n5,6)
+
+                //178
+        });
+
+        //Station 5
+        n5.setAdjacencies(new Edge[]{
+                new Edge(n4,6),
+        });
+
+        aStar.AstarSearch(n1,n5);
+
+        List<Node> path = aStar.printPath(n5);
+
+        System.out.println("Path: " + path);
     }
 
 }
